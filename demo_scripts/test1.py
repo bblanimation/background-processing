5c7b5972bb793599dc136a8e0c38811e670b02dd
bblanimation <cgear13@gmail.com>
2019-03-26 02:07:01 -0500

    Added support for 2.8 with back compat to 2.79

--- demo_scripts/test1.py
+++ demo_scripts/test1.py
@@ -5,8 +5,11 @@ import time
 # NOTE: Else, pull objects and meshes from source file using 'appendFrom(data_type:str, data_name:str)'.
 appendFrom("Object", objName)
 obj = bpy.data.objects.get(objName)
-rMod = obj.modifiers.new(obj.name + '_remesh', 'REMESH')
-m = obj.to_mesh(bpy.context.scene, True, 'PREVIEW')
+rMod = obj.modifiers.new(obj.name + "_remesh", "REMESH")
+if bpy.app.version < (2,80,0):
+    m = obj.to_mesh(bpy.context.scene, apply_modifiers=True, settings="PREVIEW")
+else:
+    m = obj.to_mesh(bpy.context.depsgraph, apply_modifiers=True)
 m.name = objName + "_remesh"
 pi = 3.14159
 
