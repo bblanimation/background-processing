bblanimation/background-processing

commit b42411ac7b06dea6134fe052e7011b143dea87cd

Fixes issue #4 (timers not specific)

classes/AddJob.py
@@ -60,7 +60,8 @@ class SCENE_OT_add_job(Operator):
             return {"CANCELLED"}
         # create timer for modal
         wm = context.window_manager
-        self._timer = wm.event_timer_add(0.5, window=context.window)
+        if self._timer is None:
+            self._timer = wm.event_timer_add(0.5, window=context.window)
         wm.modal_handler_add(self)
         return{"RUNNING_MODAL"}
 
@@ -75,6 +76,7 @@ class SCENE_OT_add_job(Operator):
                 print(retrieved_python_data)
                 wm = context.window_manager
                 wm.event_timer_remove(self._timer)
+                self._timer = None
                 return {"FINISHED"}
             if self.JobManager.job_dropped(self.job["name"]):
                 if self.JobManager.job_timed_out(self.job["name"]):
@@ -85,6 +87,7 @@ class SCENE_OT_add_job(Operator):
                     print(errormsg)
                 wm = context.window_manager
                 wm.event_timer_remove(self._timer)
+                self._timer = None
                 return {"CANCELLED"}
         return {"PASS_THROUGH"}
 
@@ -92,6 +95,7 @@ class SCENE_OT_add_job(Operator):
         self.JobManager.kill_job(self.job["name"])
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
+        self._timer = None
 
     ################################################
     # initialization method
@@ -107,5 +111,6 @@ class SCENE_OT_add_job(Operator):
     # class variables
 
     job_index = IntProperty(default=0)
+    timer = None
 
     ################################################

commit dee53eba72dffb5bf764f772220b52808685090e

Addresses issue #5, prototype bg process panel
- Added many new JobManager class methods
- Added prototype `Job Info` panel for watching job statuses, killing active jobs, etc.
- Fixed bug with timer introduced in commit b42411a
- Improved documentation

__init__.py
@@ -38,7 +38,9 @@ from .functions.common import *
 
 classes = (
     SCENE_OT_add_job,
-    VIEW3D_PT_tools_background_processing,
+    SCENE_OT_kill_job,
+    VIEW3D_PT_background_processing_tests,
+    VIEW3D_PT_background_processing_info,
 )
 
 def register():
@@ -57,7 +59,24 @@ def register():
         update=updateMaxWorkers,
         default=5)
 
+    Scene.backproc_manager_index = IntProperty(
+        name="JobManager Index",
+        description="Index for the desired JobManager instance",
+        default=-1)
+
+    Scene.backproc_job_type = EnumProperty(
+        name="Job Type",
+        description="Show info for jobs with this status",
+        items=(("ALL", "All", ""),
+               ("QUEUED", "Queued", ""),
+               ("ACTIVE", "Active", ""),
+               ("COMPLETED", "Completed", ""),
+               ("DROPPED", "Dropped", ""),),
+        default="ACTIVE")
+
 def unregister():
+    del Scene.backproc_job_type
+    del Scene.backproc_manager_index
     del Scene.backproc_max_workers
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)

classes/AddJob.py
@@ -35,7 +35,7 @@ scripts = [
 
 class SCENE_OT_add_job(Operator):
     """ Adds a job """
-    bl_idname = "scene.add_job"
+    bl_idname = "backproc.add_job"
     bl_label = "Add Job"
     bl_description = "Adds a job"
     bl_options = {'REGISTER'}
@@ -54,7 +54,7 @@ class SCENE_OT_add_job(Operator):
             return {"CANCELLED"}
         # NOTE: Set 'use_blend_file' to True to access data from the current blend file in script (False to execute script from default startup)
         # NOTE: Job will run until it is finished or until it times out (specify timeout in seconds; 0 for infinite)
-        jobAdded, msg = self.JobManager.add_job(self.job["name"], timeout=0, script=self.job["script"], use_blend_file=True, passed_data={"objName":self.obj.name, "meshName":self.obj.data.name})
+        jobAdded, msg = self.JobManager.add_job(self.job["name"], timeout=3, script=self.job["script"], use_blend_file=True, passed_data={"objName":self.obj.name, "meshName":self.obj.data.name})
         if not jobAdded:
             raise Exception(msg)
             return {"CANCELLED"}
@@ -81,8 +81,10 @@ class SCENE_OT_add_job(Operator):
             if self.JobManager.job_dropped(self.job["name"]):
                 if self.JobManager.job_timed_out(self.job["name"]):
                     self.report({"WARNING"}, "Background process '{job_name}' timed out".format(job_name=self.job["name"]))
+                elif self.JobManager.job_killed(self.job["name"]):
+                    self.report({"WARNING"}, "Background process '{job_name}' was killed".format(job_name=self.job["name"]))
                 else:
-                    self.report({"WARNING"}, "Background process '{job_name}' was dropped".format(job_name=self.job["name"]))
+                    self.report({"WARNING"}, "Background process '{job_name}' failed".format(job_name=self.job["name"]))
                     errormsg = self.JobManager.get_issue_string(self.job["name"])
                     print(errormsg)
                 wm = context.window_manager
@@ -111,6 +113,6 @@ class SCENE_OT_add_job(Operator):
     # class variables
 
     job_index = IntProperty(default=0)
-    timer = None
+    _timer = None
 
     ################################################

classes/JobManager.py
@@ -163,6 +163,7 @@ class JobManager():
         self.passed_data[job] = passed_data
         self.uses_blend_file[job] = use_blend_file
         self.job_timeouts[job] = timeout
+        self.job_statuses[job] = {"started":False, "returncode":None, "stdout":None, "stderr":None, "start_time":time.time(), "end_time":None, "attempts":0, "timed_out":False}
         # save the active blend file to be used in Blender instance
         if use_blend_file and (not os.path.exists(self.blendfile_paths[job]) or overwrite_blend):
             try:
@@ -183,14 +184,14 @@ class JobManager():
 
     def start_job(self, job:str, debug_level:int=0):
         # send job string to background blender instance with subprocess
-        attempts = 1 if job not in self.job_statuses.keys() else (self.job_statuses[job]["attempts"] + 1)
         binary_path = bpy.app.binary_path
         blendfile_path = self.blendfile_paths[job] if self.uses_blend_file[job] else ""
         temp_job_path = self.job_paths[job]
         # TODO: Choose a better exit code than 155
         thread_func = "'%(binary_path)s' '%(blendfile_path)s' -b --python-exit-code 155 -P '%(temp_job_path)s'" % locals()
         self.job_processes[job] = subprocess.Popen(thread_func, stdout=subprocess.PIPE if debug_level in (0, 2) else None, stderr=subprocess.PIPE if debug_level < 2 else None, shell=True)
-        self.job_statuses[job] = {"returncode":None, "stdout":None, "stderr":None, "start_time":time.time(), "end_time":None, "attempts":attempts}
+        self.job_statuses[job]["started"] = True
+        self.job_statuses[job]["attempts"] += 1
         self.retrieved_data[job] = {"retrieved_data_blocks":None, "retrieved_python_data":None}
         print("JOB STARTED:  ", job)
 
@@ -208,6 +209,7 @@ class JobManager():
         # check if job has exceeded the time limit
         elif self.job_timeouts[job] > 0 and time.time() - job_status["start_time"] > self.job_timeouts[job]:
             self.kill_job(job)
+            self.job_statuses[job]["timed_out"] = True
         job_process = self.job_processes[job]
         job_process.poll()
         # check if job process still running
@@ -280,8 +282,27 @@ class JobManager():
     def get_job_names(self):
         return self.jobs
 
+    def get_queued_job_names(self):
+        return [job for job in self.jobs if not self.job_started(job)]
+
+    def get_active_job_names(self):
+        return [job for job in self.jobs if self.job_started(job) and not (self.job_complete(job) or self.job_dropped(job))]
+
+    def get_completed_job_names(self):
+        return [job for job in self.jobs if self.job_complete(job)]
+
+    def get_dropped_job_names(self):
+        return [job for job in self.jobs if self.job_dropped(job)]
+
     def get_job_status(self, job:str):
-        return self.job_statuses[job]
+        if not self.job_started(job):
+            return "QUEUED"
+        elif self.job_complete(job):
+            return "COMPLETED"
+        elif self.job_dropped(job):
+            return "DROPPED"
+        else:
+            return "ACTIVE"
 
     def get_retrieved_python_data(self, job:str):
         return self.retrieved_data[job]["retrieved_python_data"]
@@ -295,8 +316,8 @@ class JobManager():
             errormsg = "\nJob '%(job)s' timed out\n\n" % locals()
         else:
             errormsg = "\n------ ISSUE WITH BACKGROUND PROCESSOR ------\n\n"
-            stderr = self.get_job_status(job)["stderr"]
-            stdout = self.get_job_status(job)["stdout"]
+            stderr = self.job_statuses[job]["stderr"]
+            stdout = self.job_statuses[job]["stdout"]
             errormsg += "[stderr]\n" if len(stderr) > 0 else "[stdout]\n"
             for line in stderr if len(stderr) > 0 else stdout:
                 if line.startswith("\r"):
@@ -307,16 +328,23 @@ class JobManager():
         return errormsg
 
     def job_started(self, job:str):
-        return job in self.job_statuses.keys()
+        return self.job_statuses[job]["started"]
 
     def job_complete(self, job:str):
-        return job in self.job_statuses and self.job_statuses[job]["returncode"] == 0
+        """ returns True if job was completed successfully (return code 0) """
+        return self.job_statuses[job]["returncode"] == 0
 
     def job_dropped(self, job:str):
-        return job in self.job_statuses and self.job_statuses[job]["attempts"] == self.max_attempts and self.job_statuses[job]["returncode"] not in (None, 0)
+        """ returns True if job was killed, timed out, or encountered an error """
+        return self.job_statuses[job]["attempts"] == self.max_attempts and self.job_statuses[job]["returncode"] not in (None, 0)
+
+    def job_killed(self, job:str):
+        """ returns True if job was killed or timed out """
+        return self.job_statuses[job]["returncode"] == -9
 
     def job_timed_out(self, job:str):
-        return job in self.job_statuses and self.job_statuses[job]["attempts"] == self.max_attempts and self.job_statuses[job]["returncode"] == -9
+        """ returns True if job timed out """
+        return self.job_statuses[job]["timed_out"] and self.job_statuses[job]["attempts"] == self.max_attempts
 
     def jobs_complete(self):
         for job in self.jobs:
@@ -332,6 +360,7 @@ class JobManager():
             self.cleanup_job(job)
 
     def cleanup_job(self, job):
+        """ removes job from all JobManager data structures """
         assert job in self.jobs
         self.jobs.remove(job)
         del self.passed_data[job]
@@ -353,9 +382,9 @@ class JobManager():
         return len(self.job_processes)
 
     def num_completed_jobs(self):
-        return [self.job_complete(job) for job in self.jobs].count(True)
+        return self.get_completed_job_names().count(True)
 
     def num_dropped_jobs(self):
-        return [self.job_dropped(job) for job in self.jobs].count(True)
+        return self.get_dropped_job_names().count(True)
 
     ###################################################

classes/KillJob.py
@@ -1,0 +1,62 @@
+# Copyright (C) 2019 Christopher Gearhart
+# chris@bblanimation.com
+# http://bblanimation.com/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# System imports
+# NONE!
+
+# Blender imports
+import bpy
+from bpy.types import Operator
+
+# Addon imports
+from .JobManager import *
+
+
+class SCENE_OT_kill_job(Operator):
+    """ Kills a job """
+    bl_idname = "backproc.kill_job"
+    bl_label = "Kill Job"
+    bl_description = "Kills a job"
+    bl_options = {'REGISTER'}
+
+    ################################################
+    # Blender Operator methods
+
+    # @classmethod
+    # def poll(self, context):
+    #     """ ensures operator can execute (if not, returns false) """
+    #     return bpy.context.object is not None
+
+    def execute(self, context):
+        if self.job_name not in self.JobManager.get_active_job_names():
+            return {"CANCELLED"}
+        self.JobManager.kill_job(self.job_name)
+        return {"FINISHED"}
+
+    ################################################
+    # initialization method
+
+    def __init__(self):
+        self.JobManager = JobManager.get_instance(-1)
+        self.JobManager.max_workers = 5
+
+    ###################################################
+    # class variables
+
+    job_name = StringProperty()
+
+    ################################################

classes/__init__.py
@@ -17,3 +17,4 @@
 
 from .AddJob import *
 from .JobManager import *
+from .KillJob import *

ui/__init__.py
@@ -15,57 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# System imports
-import os
-import subprocess
-
-# Blender imports
-import bpy
-from bpy.types import Operator, Panel
-
-# Addon imports
-from ..classes.JobManager import *
-from ..functions.common import *
-
-
-class VIEW3D_PT_tools_background_processing(Panel):
-    bl_space_type  = "VIEW_3D"
-    bl_region_type = "UI" if b280() else "TOOLS"
-    bl_label       = "Background Processing"
-    bl_idname      = "VIEW3D_PT_tools_background_processing"
-    bl_context     = "objectmode"
-    bl_category    = "BackProc"
-
-    @classmethod
-    def poll(self, context):
-        return True
-
-    def draw(self, context):
-        layout = self.layout
-        scn = context.scene
-        manager = JobManager.get_instance()
-
-        col = layout.column(align=True)
-        row = col.row(align=True)
-        row.operator("scene.add_job", text="Add Job 1").job_index = 0
-        row = col.row(align=True)
-        row.operator("scene.add_job", text="Add Job 2").job_index = 1
-        row = col.row(align=True)
-        row.operator("scene.add_job", text="Add Job 3").job_index = 2
-        row = col.row(align=True)
-        row.operator("scene.add_job", text="Add Job 4 (timeout)").job_index = 3
-        col = layout.column(align=True)
-        row = col.row(align=True)
-        row.prop(scn, "backproc_max_workers")
-        col = layout.column(align=True)
-        col.scale_y = 0.7
-        row = col.row(align=True)
-        row.label(text="Pending Jobs: " + str(manager.num_pending_jobs()))
-        row = col.row(align=True)
-        row.label(text="Running Jobs: " + str(manager.num_running_jobs()))
-        row = col.row(align=True)
-        row.label(text="Completed Jobs: " + str(manager.num_completed_jobs()))
-        row = col.row(align=True)
-        row.label(text="Dropped Jobs: " + str(manager.num_dropped_jobs()))
-        row = col.row(align=True)
-        row.label(text="Available Workers: " + str(manager.num_available_workers()))
+from .bgtests import *
+from .bginfo import *

ui/bginfo.py
@@ -1,0 +1,100 @@
+# Copyright (C) 2019 Christopher Gearhart
+# chris@bblanimation.com
+# http://bblanimation.com/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# System imports
+import os
+import subprocess
+
+# Blender imports
+import bpy
+from bpy.types import Operator, Panel
+
+# Addon imports
+from ..classes.JobManager import *
+from ..functions.common import *
+
+
+class VIEW3D_PT_background_processing_info(Panel):
+    bl_space_type  = "VIEW_3D"
+    bl_region_type = "UI" if b280() else "TOOLS"
+    bl_label       = "Job Info"
+    bl_idname      = "VIEW3D_PT_background_processing_info"
+    bl_context     = "objectmode"
+    bl_category    = "BackProc"
+
+    @classmethod
+    def poll(self, context):
+        return True
+
+    def draw(self, context):
+        layout = self.layout
+        scn = context.scene
+        # get the desired JobManager instance (in this case, it uses the scene
+        # property which can be exposed to the interface with the commented out
+        # code a few lines further down)
+        manager = JobManager.get_instance(scn.backproc_manager_index)
+
+        # NOTE: Include the following code if using multiple instances of
+        #       JobManager to allow the user to choose the JobManager index.
+        # col = layout.column(align=True)
+        # row = col.row(align=True)
+        # row.prop(scn, "backproc_manager_index")
+
+        col = layout.column(align=True)
+        row = col.row(align=True)
+        row.prop(scn, "backproc_job_type")
+
+        col = layout.column(align=True)
+        col.scale_y = 0.7
+        row = col.row(align=True)
+        row.label(text="Pending Jobs: " + str(manager.num_pending_jobs()))
+        row = col.row(align=True)
+        row.label(text="Running Jobs: " + str(manager.num_running_jobs()))
+        row = col.row(align=True)
+        row.label(text="Completed Jobs: " + str(manager.num_completed_jobs()))
+        row = col.row(align=True)
+        row.label(text="Dropped Jobs: " + str(manager.num_dropped_jobs()))
+        row = col.row(align=True)
+        row.label(text="Available Workers: " + str(manager.num_available_workers()))
+
+        if scn.backproc_job_type == "QUEUED":
+            jobs = manager.get_queued_job_names()
+        elif scn.backproc_job_type == "ACTIVE":
+            jobs = manager.get_active_job_names()
+        elif scn.backproc_job_type == "COMPLETED":
+            jobs = manager.get_completed_job_names()
+        elif scn.backproc_job_type == "DROPPED":
+            jobs = manager.get_dropped_job_names()
+        else:
+            jobs = manager.get_job_names()
+        col = layout.column(align=True)
+        i = 0
+        for job in sorted(jobs):
+            i += 1
+            row = col.row(align=True)
+            row.label(text="%(i)s) %(job)s" % locals())
+            row = col.row(align=True)
+            job_status = manager.get_job_status(job)
+            if job_status == "ACTIVE":
+                split = layout_split(row, factor=0.85)
+                col1 = split.column(align=True)
+                col1.label(text="Status: " + job_status.capitalize())
+                col1 = split.column(align=True)
+                col1.operator("backproc.kill_job", text="", icon="CANCEL").job_name = job
+            else:
+                row.label(text="Status: " + job_status.capitalize())
+            layout.separator()

ui/bgtests.py
@@ -1,0 +1,59 @@
+# Copyright (C) 2019 Christopher Gearhart
+# chris@bblanimation.com
+# http://bblanimation.com/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# System imports
+import os
+import subprocess
+
+# Blender imports
+import bpy
+from bpy.types import Operator, Panel
+
+# Addon imports
+from ..classes.JobManager import *
+from ..functions.common import *
+
+
+class VIEW3D_PT_background_processing_tests(Panel):
+    bl_space_type  = "VIEW_3D"
+    bl_region_type = "UI" if b280() else "TOOLS"
+    bl_label       = "Run Tests"
+    bl_idname      = "VIEW3D_PT_background_processing_tests"
+    bl_context     = "objectmode"
+    bl_category    = "BackProc"
+
+    @classmethod
+    def poll(self, context):
+        return True
+
+    def draw(self, context):
+        layout = self.layout
+        scn = context.scene
+        manager = JobManager.get_instance()
+
+        col = layout.column(align=True)
+        row = col.row(align=True)
+        row.operator("backproc.add_job", text="Add Job 1").job_index = 0
+        row = col.row(align=True)
+        row.operator("backproc.add_job", text="Add Job 2").job_index = 1
+        row = col.row(align=True)
+        row.operator("backproc.add_job", text="Add Job 3").job_index = 2
+        row = col.row(align=True)
+        row.operator("backproc.add_job", text="Add Job 4 (timeout)").job_index = 3
+        col = layout.column(align=True)
+        row = col.row(align=True)
+        row.prop(scn, "backproc_max_workers")